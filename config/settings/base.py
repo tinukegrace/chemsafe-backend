f7e5b02ab9137660ddcdafc38becac81281a50fc
tinuke <oguntinuke@gmail.com>
2026-08-10 03:42:50 +0100

    Configure PostgreSQL using DATABASE_URL

--- config/settings/base.py
+++ config/settings/base.py
@@ -72,14 +72,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST", default="localhost"),
-        "PORT": env("DB_PORT", default="5432"),
-    }
+    "default": env.db("DATABASE_URL"),
 }
 
 AUTH_USER_MODEL = "accounts.User"
